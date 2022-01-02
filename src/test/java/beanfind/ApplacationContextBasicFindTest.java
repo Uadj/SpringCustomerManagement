aa51eb3dab3e357a0c3d9190dd929a3deb974168
dgw <30551889+Uadj@users.noreply.github.com>
2022-01-03 04:28:58 +0900

    Bean find Test added

--- src/test/java/beanfind/ApplacationContextBasicFindTest.java
+++ src/test/java/beanfind/ApplacationContextBasicFindTest.java
@@ -1,7 +1,10 @@
+package beanfind;
+
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import project.AppConfig;
 import project.MemberService;
@@ -23,6 +26,18 @@ public class ApplacationContextBasicFindTest {
     void findBeanByType(){
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2(){
+        MemberServiceImpl memberService = ac.getBean("memberService",MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("빈 이름으로 조회x")
+    void findBeanByNameX(){
+     //   MemberService memberService = ac.getBean("xxxx", MemberService.class);
+        org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class,()->ac.getBean("xxxx",MemberService.class));
     }
 }
 
